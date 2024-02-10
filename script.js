d4d2c1c17ea3455a7a152eb1121c0dd79e7e41b8
DanielaSD <danielasestelo42@gmail.com>
2024-02-10 22:37:59 +0100

    refactorizacion del código

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
-//const APIKEY="sk-rTUm65c3fe816e1324068";
-//const APIKEY="sk-yoVU65c4f724016454078"
-const baseURL = "https://perenual.com/api/";
+"use strict";
+const APIKEY = "sk-rTUm65c3fe816e1324068";
+//const APIKEY="sk-yoVU65c4f724016454078";
+const BASE_URL = "https://perenual.com/api/";
 const selectedPlantKind = document.getElementById("selectedPlantKind");
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch(baseURL + "species-list?key=" + APIKEY)
+    fetch(BASE_URL
+        + "species-list?key=" + APIKEY)
         .then(response => response.json())
         .then(response => {
             const species = response.data;
@@ -22,92 +23,98 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 });
 
-let selectedOption = document.getElementById("selectMethod");
-let selectedSpecies = document.getElementById("selectedPlantKind");
+const selectedOption = document.getElementById("selectMethod");
+const selectedSpecies = document.getElementById("selectedPlantKind");
 
-const img = document.getElementById("plantImg");
-const p = document.getElementById("scientificName");
-
+//simula que cambia una especie en caso de que cambies de método de llamada, así se volverá a hacer una llamada a la api
 selectedOption.addEventListener("change", () => {
-    let event = new Event('change');
-
-    // Dispatch it.
+    const event = new Event('change');
     selectedSpecies.dispatchEvent(event);
 });
 
-const defaultImg=document.getElementById("defaultImg");
+const defaultImg = document.getElementById("defaultImg");
+const OPTION_IMG_ASYNC = "optionImgAsync";
+const OPTION_IMG_PROMISE = "optionImgPromise";
+const OPTION_TXT_ASYNC = "optionTxAsync";
+const OPTION_TXT_PROMISE = "optionTxPromise";
+const img = document.getElementById("plantImg");
+const p = document.getElementById("scientificName");
+const errorMessage = document.getElementById("errorMessage");
+console.log("DDDDDDddddd" + selectedSpecies.value);
 selectedPlantKind.addEventListener("change", () => {
-    console.log("Bien3");
     p.style = "display:none";
     img.style = "display:none";
-    console.log(selectedSpecies)
-    if (selectedOption.value === "optionImgAsync") {
+    errorMessage.style = "display:none";
+    if (selectedOption.value === OPTION_IMG_ASYNC && selectedSpecies.value) {
 
         const apiAsync = async () => {
             try {
-                let response = await fetch(baseURL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY);
+                let response = await fetch(BASE_URL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY);
+
                 response = await response.json();
                 const img = document.getElementById("plantImg");
                 img.setAttribute("src", response.default_image.small_url);
                 img.style = "display:block;";
-                defaultImg.style="display:none";
+                defaultImg.style = "display:none";
             } catch (error) {
-                console.error("Error fetching image asynchronously:", error);
+                errorMessage.style = "display:block";
+                console.log("Error fetching image asynchronously:", error);
             }
         }
         apiAsync();
     }
-    else if (selectedOption.value === "optionImgPromise") {
+    else if (selectedOption.value === OPTION_IMG_PROMISE && selectedSpecies.value) {
         p.style = "display:none";
-        fetch(baseURL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY)
-
-            .then(response => response.json())
-            .then(response => {
-                const img = document.getElementById("plantImg");
-                img.setAttribute("src", response.default_image.small_url);
-                img.style = "display:block;"
-                defaultImg.style="display:none";
-            })
-            .catch(error => {
-                console.error("Error fetching image with promise:", error);
-            });
+            fetch(BASE_URL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY)
+                .then(response => response.json())
+                .then(response => {
+                    const img = document.getElementById("plantImg");
+                    img.setAttribute("src", response?.default_image.small_url);
+                    img.style = "display:block;"
+                    defaultImg.style = "display:none";
+                })
+                .catch(error => {
+                    errorMessage.style = "display:block";
+                    console.log("Error fetching image with promise:", error);
+                });
+        
 
     }
-    else if (selectedOption.value === "optionTxAsync") {
+    else if (selectedOption.value === OPTION_TXT_ASYNC && selectedSpecies.value) {
         img.style = "display:none";
         const apiAsync = async () => {
             try {
-                let response = await fetch(baseURL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY);
+                let response = await fetch(BASE_URL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY);
                 response = await response.json();
                 document.getElementById("scientificName").textContent = response.scientific_name;
                 p.style = "display:block;"
                 document.getElementById("scientificName").innerHTML = `El nombre científico de la especie seleccionada es: ${response.scientific_name}`;
-                defaultImg.style="display:none";
+                defaultImg.style = "display:none";
             } catch (error) {
-                console.error("Error fetching scientific name with promise:", error);
+                errorMessage.style = "display:block";
+                console.log("Error fetching scientific name with promise:", error);
             }
         }
-        apiAsync();
+       apiAsync();
 
     }
-    else if (selectedOption.value === "optionTxPromise") {
+    else if (selectedOption.value === OPTION_TXT_PROMISE && selectedSpecies.value) {
         img.style = "display:none";
-        fetch(baseURL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY)
-
-            .then(response => response.json())
-            .then(response => {
-                document.getElementById("scientificName").textContent = response.scientific_name;
-                console.log(response);
-                p.style = "display:block;"
-                document.getElementById("scientificName").innerHTML = `El nombre científico de la especie seleccionada es: ${response.scientific_name}`;
-                defaultImg.style="display:none";
-            })
-            .catch(error => {
-                console.error("Error fetching scientific name with promise:", error);
-            });
-
+        
+            fetch(BASE_URL + "species/details/" + selectedSpecies.value + "?key=" + APIKEY)
+                .then(response => response.json())
+                .then(response => {
+                    document.getElementById("scientificName").textContent = response.scientific_name;
+                    p.style = "display:block;"
+                    document.getElementById("scientificName").innerHTML = `El nombre científico de la especie seleccionada es: ${response.scientific_name}`;
+                    defaultImg.style = "display:none";
+                })
+                .catch(error => {
+                    console.log("Error fetching scientific name with promise:", error);
+                    errorMessage.style = "display:block";
+                });
     }
     else {
-        alert("Debe seleccionar una opción");
+        defaultImg.style="display:block";
     }
 });
